e87e7270ac5f627ead2a110344e8c8f5f4bbd8dc
Cezary Zysk <cezaryy.z97@gmail.com>
2021-05-03 15:40:56 +0200

    Fix bug google authentication

--- EmployeManagment/Controllers/AccountController.cs
+++ EmployeManagment/Controllers/AccountController.cs
@@ -95,30 +95,42 @@ namespace EmployeManagment.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> Login(LoginViewModel model,string returnUrl )
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
         {
             if (ModelState.IsValid)
             {
-             
-                var result = await signInManager.PasswordSignInAsync(model.Email,model.Password, 
-                                                                    model.RememberMe,false);
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, false);
 
                 if (result.Succeeded)
                 {
-                    // return url jesli sie zalogujesz powroc do miejsca w ktorym poproszono cie o zalogowanie
                     if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
                     {
                         return Redirect(returnUrl);
-                    } 
+                    }
                     else
                     {
                         return RedirectToAction("index", "home");
                     }
-
                 }
-                    ModelState.AddModelError(string.Empty,"Invalid Login Attempt" );
-                
+                else
+                {
+                    // aby nie wyslac nulla do externallogins
+                    LoginViewModel failmodel = new LoginViewModel
+                    {
+                        Email = model.Email,
+                        Password = model.Password,
+                        RememberMe = model.RememberMe,
+                        ReturnUrl = returnUrl,
+                        ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+                    };
+                    ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                    return View(failmodel);
+                }
+
+
             }
+
             return View(model);
         }
         [AllowAnonymous]
@@ -153,7 +165,7 @@ namespace EmployeManagment.Controllers
                 return View("Login", loginViewModel);
             }
 
-            // Get the login information about the user from the external login provider
+            // pobierz info o logowaniu
             var info = await signInManager.GetExternalLoginInfoAsync();
             if (info == null)
             {
@@ -163,8 +175,7 @@ namespace EmployeManagment.Controllers
                 return View("Login", loginViewModel);
             }
 
-            // If the user already has a login (i.e if there is a record in AspNetUserLogins
-            // table) then sign-in the user with this external login provider
+ 
             var signInResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider,
                 info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
 
@@ -172,16 +183,15 @@ namespace EmployeManagment.Controllers
             {
                 return LocalRedirect(returnUrl);
             }
-            // If there is no record in AspNetUserLogins table, the user may not have
-            // a local account
+            
             else
             {
-                // Get the email claim value
+                
                 var email = info.Principal.FindFirstValue(ClaimTypes.Email);
 
                 if (email != null)
                 {
-                    // Create a new user without password if we do not have a user already
+                   
                     var user = await userManager.FindByEmailAsync(email);
 
                     if (user == null)
@@ -195,16 +205,16 @@ namespace EmployeManagment.Controllers
                         await userManager.CreateAsync(user);
                     }
 
-                    // Add a login (i.e insert a row for the user in AspNetUserLogins table)
+                  
                     await userManager.AddLoginAsync(user, info);
                     await signInManager.SignInAsync(user, isPersistent: false);
 
                     return LocalRedirect(returnUrl);
                 }
 
-                // If we cannot find the user email we cannot continue
+              
                 ViewBag.ErrorTitle = $"Email claim not received from: {info.LoginProvider}";
-                ViewBag.ErrorMessage = "Please contact support on Pragim@PragimTech.com";
+                ViewBag.ErrorMessage = "Please contact support on cezaryy.z97@gmail.com";
 
                 return View("Error");
             }
